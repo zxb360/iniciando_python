e0dc7b2493e94d62eadec8e0c52c2737f9e577da
zxb360 <jaederazevedo@pop-os.localdomain>
2022-06-26 15:46:43 -0300

    manipulando string com (.find())url python

---
+++ manipulando_string.py
@@ -0,0 +1,12 @@
+url ="bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+#base para busca de parâmetro
+url_base = url.find("?");
+url_inicial = url[:url_base];
+url_final = url[url_base + 1:];
+
+#encontrando valores com parametros
+
+print(url_inicial)
+print(url_final)
+
+
