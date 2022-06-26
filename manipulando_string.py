19f67e7e7678d29d3e8d7e2d60202cdeb566754a
zxb360 <jaederazevedo@pop-os.localdomain>
2022-06-26 16:04:09 -0300

    manipulando string

--- manipulando_string.py
+++ manipulando_string.py
@@ -5,6 +5,8 @@ url_inicial = url[:url_base];
 url_final = url[url_base + 1:];
 
 #encontrando valores com parametros
+busca = "moedaDestino"
+parametro_base = url_final.find("&")
 
 print(url_inicial)
 print(url_final)
